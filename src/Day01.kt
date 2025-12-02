3aac348619cb3564c9616bd7c634a689e2fd1fe8
Chris Gual <cgual@omnidian.com>
2025-12-01 20:54:05 -0800

    Day 1: Parts 1 & 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,49 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        input.fold(Pair<Int, Int>(50, 0)) { acc, line ->
+            require(line.length >= 2) { "Line should have at least 2 characters" }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+            val direction = line[0]
+            val degree = line.substring(1, line.length).toInt()
+
+            val position: Int = (when(direction) {
+                'R' -> degree
+                'L' -> (100 - degree) // all lefts are just right by 100 - left
+                else -> throw IllegalArgumentException("Invalid line: $line")
+            } + acc.first) % 100
+
+            Pair(position, acc.second + if (position == 0) 1 else 0)
+        }.second
+
+    fun part2(input: List<String>): Int =
+        input.fold(Pair<Int, Int>(50, 0)) { acc, line ->
+            require(line.length >= 2) { "Line should have at least 2 characters" }
+
+            val degree = line.substring(1, line.length).toInt()
+            val direction = when (line[0]) {
+                'R' -> 1
+                'L' -> -1
+                else -> throw IllegalArgumentException("Invalid line: $line")
+            }
+
+            generateSequence(acc) { it
+                ((it.first + direction + 100) % 100).let { position: Int ->
+                    Pair(
+                        position,
+                        it.second + if (position == 0) 1 else 0
+                    )
+                }
+            }.take(degree + 1)
+             .last()
+        }.second
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    check(part1(listOf("R50")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
